33f3ded460adfaa4e0c49efcdc55d8b4394f08e0
iCelf08 <chfirstly@gmail.com>
2023-11-26 23:01:26 +0100

    8

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Square module"""
+
+class Square:
+    """Defines square"""
+
+    def __init__(self, size=0):
+        """ initialises
+            Args :
+                size : size of square
+           
+       """
+   @property
+   def size(self):
+       """ property for size in square 
+     Raises :
+              TypeError : if size is not an integer
+              ValueError : if size less than 0
+        """
+        return self.__size
+
+   @size.setter
+   def size(self, value):
+         self.size = value
+            if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+
+   def area(self):
+        """calculates area of square
+
+        Returns :
+               size squared
+        """
+        return self.__size ** 2
