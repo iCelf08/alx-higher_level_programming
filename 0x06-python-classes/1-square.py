3b29cbce33eb619a57d90b7b528ae5deb8fe12eb
iCelf08 <chfirstly@gmail.com>
2023-11-26 22:27:01 +0100

    u

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,9 +5,10 @@ class Square:
     """Define square."""
 
     def __init__(self, size):
-    """initialiser
+    """initialiser.
+
     Args:
        size: size of square
     """
 
-          self.__size = size
+        self.__size = size
