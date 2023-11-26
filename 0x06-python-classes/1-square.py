6a885295423f81d9f7f8b681340a4edfb52dbc81
iCelf08 <chfirstly@gmail.com>
2023-11-26 22:29:36 +0100

    iden

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,10 +5,10 @@ class Square:
     """Define square."""
 
     def __init__(self, size):
-    """initialiser.
+        """initialiser.
 
-    Args:
-       size: size of square
-    """
+        Args:
+           size: size of square
+        """
 
         self.__size = size
