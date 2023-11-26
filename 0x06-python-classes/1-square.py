c626d3f6b65340824fcf060eb37be3ab0dd11155
iCelf08 <chfirstly@gmail.com>
2023-11-26 21:48:40 +0100

    classes objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" square with size """
+
+class square:
+    """ square"""
+    def __init__ (self, size):
+    """ initialiser
+    Args:
+     size: size of square
+    """
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" square module """
+class square:
+    """ defines square """
+    def __init__(self, size=0):
+        """ initialises
+            Args :
+                size : size of square
+            Raises :
+         TypeError : if size is not an integer
+         ValueError : if size less than 0
+        """ 
+        
+        if not isinstace(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """ calculates area of square
+            Returns : 
+                   size squared
+        """
+        return self.__size ** 2
+    
