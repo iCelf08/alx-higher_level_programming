765f36b050209dbe6a265b3f3f65854f43313d8c
iCelf08 <chfirstly@gmail.com>
2023-11-27 18:31:51 +0100

    1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Rectongle module"""
+
+class Rectangle:
+    """Defines Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Construct:
+        Arg:
+            width : width of rectangle
+            height : height of rectangle
+        """
+        self.__width = width
+        self.__height = height
+        
+    
+    @property
+    """property instance width
+    Return:
+         value of width
+    """
+    def width(self):
+        """Property getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Property setter"""
+        if not isinstance(value, int):
+            raise TypeError('width must be integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+
+    @property
+    """property instance hieght
+    Return:
+         value of height
+    """
+    def height(self):
+        """Property getter for height"""
+        return self.__height
+
+    @height.setter:
+
+    def height(self, value):
+        """Property setter for height"""
+        if not isinstance(value, int):
+            raise TypeError('height must be integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
