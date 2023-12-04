d5c9846a5b33df50986cab4281d30c4bf0fa62a7
iCelf08 <chfirstly@gmail.com>
2023-12-04 17:11:44 +0100

    99

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module for Rectangle"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeomtry):
+    """ Rectangle inherits from BaseGeometry class"""
+
+    def __init__(self, width, height):
+        """ Constructor """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """method for counting area"""
+         return self.width * self.height
+
+    def __str__(self):
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
