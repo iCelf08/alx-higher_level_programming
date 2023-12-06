50084d2d02598219a14cce4a66bee5a3308572c0
iCelf08 <chfirstly@gmail.com>
2023-12-06 16:48:50 +0100

    dde

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines a Pascal's Triangle function."""
+
+
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """writs objct to txt file"""
     with open(filename, 'w', encoding='utf-8') as file:
-        json.dumps(my_obj, file)
+        json.dump(my_obj, file)
