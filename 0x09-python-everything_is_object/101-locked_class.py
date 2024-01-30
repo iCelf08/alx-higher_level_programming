59327d3062cb43454d4d17fb01274dab5521eecb
iCelf08 <chfirstly@gmail.com>
2024-01-30 15:50:30 +0100

    jj

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,6 +3,7 @@ class LockedClass:
     """ Class with no class or object attribut"""
     __slots__ = ("first_name")
 
-    def __init__(self):
-        """ new instnce """
-        self.first_name = "first_name"
+    def __setattr__(self, name, value):
+        if name != "first_name":
+            raise AttributeError("LockedClass has no attributes")
+        self.__dict__[name] = value
