5132fd12783060230e3473d8eaf3f5cdc0776fca
c.elfarji@yanvision.ma <c.elfarji@yanvision.ma>
2024-08-23 22:15:27 +0100

    Gg3

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+# Sends a request to a URL passed as the first argument and displays only the status code of the response
+
+curl -s -o /dev/null -w "%{http_code}" "$1"
